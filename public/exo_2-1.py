7b741ed23ff2886d1a4b6fbf4b5b025dccb4410c
bernard hugueney <gitlab.com@bernard-hugueney.org>
2020-08-29 21:24:04 +0200

    removing leftover from flat tangle publishing

--- public/exo_2-1.py
+++
@@ -1,7 +0,0 @@
-#%load solutions/exo_2.py
-def squareList(n):
-    res = []
-    for it in range(1, n+1):
-        None
-        # Écrire le code ici
-    return res
--- public/exo_test-1.py
+++
@@ -1,7 +0,0 @@
-#%load solutions/exo_test.py
-def squareList(n):
-    res = []
-    for it in range(1, n+1):
-        None
-        # Écrire le code ici
-    return res
